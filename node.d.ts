755d10d5ad823ff6b88952a7cb4fc5641b2c4522
nin-jin <nin-jin@users.noreply.github.com>
2023-12-11 07:09:55 +0000

    Deploying to gh-pages from @ hyoo-ru/match.hyoo.ru@33d5888e5e7515d241b2aca47594e1dd77d2adbd 🚀

--- node.d.ts
+++ node.d.ts
@@ -4368,7 +4368,13 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    class $mol_icon_github_circle extends $mol_icon {
+    class $mol_icon_script extends $mol_icon {
+        path(): string;
+    }
+}
+
+declare namespace $ {
+    class $mol_icon_script_text extends $mol_icon {
         path(): string;
     }
 }
@@ -4377,7 +4383,7 @@ declare namespace $ {
     class $mol_link_source extends $mol_link {
         hint(): string;
         sub(): readonly any[];
-        Icon(): $mol_icon_github_circle;
+        Icon(): $mol_icon_script_text;
     }
 }
 
