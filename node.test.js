b67384cf4b1c4c64d42256f3c3d30277852e9a3a
nin-jin <nin-jin@users.noreply.github.com>
2024-01-06 07:08:23 +0000

    Deploying to gh-pages from @ hyoo-ru/match.hyoo.ru@33d5888e5e7515d241b2aca47594e1dd77d2adbd 🚀

--- node.test.js
+++ node.test.js
@@ -12401,6 +12401,9 @@ var $;
             uri_base() {
                 return $mol_dom_context.document.location.href;
             }
+            uri_base_abs() {
+                return new URL(this.uri_base(), $mol_dom_context.document.location.href);
+            }
             uri_resolve(uri) {
                 if (/^(\w+script+:)+/.test(uri))
                     return null;
@@ -12415,7 +12418,7 @@ var $;
                     return this.$.$mol_state_arg.link(params);
                 }
                 try {
-                    const url = new URL(uri, this.uri_base());
+                    const url = new URL(uri, this.uri_base_abs());
                     return url.toString();
                 }
                 catch (error) {
@@ -12548,6 +12551,9 @@ var $;
         __decorate([
             $mol_mem_key
         ], $mol_text.prototype, "grid_cell_text", null);
+        __decorate([
+            $mol_mem
+        ], $mol_text.prototype, "uri_base_abs", null);
         __decorate([
             $mol_mem_key
         ], $mol_text.prototype, "uri_resolve", null);
