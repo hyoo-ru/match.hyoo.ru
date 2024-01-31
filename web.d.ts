cc33843fba30eeffa12675c13efc719cec703c97
nin-jin <nin-jin@users.noreply.github.com>
2024-01-31 07:09:45 +0000

    Deploying to gh-pages from @ hyoo-ru/match.hyoo.ru@33d5888e5e7515d241b2aca47594e1dd77d2adbd 🚀

--- web.d.ts
+++ web.d.ts
@@ -2039,15 +2039,8 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    function $mol_diff_path<Item>(...paths: Item[][]): {
-        prefix: Item[];
-        suffix: Item[][];
-    };
-}
-
-declare namespace $ {
-    class $mol_error_mix extends Error {
-        errors: Error[];
+    class $mol_error_mix extends AggregateError {
+        name: string;
         constructor(message: string, ...errors: Error[]);
         toJSON(): string;
     }
@@ -2055,6 +2048,7 @@ declare namespace $ {
 
 declare namespace $ {
     class $mol_data_error extends $mol_error_mix {
+        name: string;
     }
 }
 
