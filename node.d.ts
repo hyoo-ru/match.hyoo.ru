6d6660397336de98f9059d723d4bc10609091fb7
nin-jin <nin-jin@users.noreply.github.com>
2025-08-27 07:21:43 +0000

    Deploying to gh-pages from @ hyoo-ru/match.hyoo.ru@edc23323b5f43e26e5f70de303f5e5a2763209a0 🚀

--- node.d.ts
+++ node.d.ts
@@ -160,7 +160,7 @@ declare namespace $ {
     function $mol_dev_format_native(obj: any): any[];
     function $mol_dev_format_auto(obj: any): any[];
     function $mol_dev_format_element(element: string, style: object, ...content: any[]): any[];
-    function $mol_dev_format_span(style: object, ...content: any[]): any[];
+    let $mol_dev_format_span: (style: object, ...content: any[]) => any[];
     let $mol_dev_format_div: (style: object, ...content: any[]) => any[];
     let $mol_dev_format_ol: (style: object, ...content: any[]) => any[];
     let $mol_dev_format_li: (style: object, ...content: any[]) => any[];
@@ -3473,6 +3473,10 @@ declare namespace $.$$ {
 }
 
 declare namespace $ {
+}
+
+declare namespace $ {
+    function $mol_hash_numbers(buff: ArrayLike<number>, seed?: number): number;
 }
 
 declare namespace $ {
