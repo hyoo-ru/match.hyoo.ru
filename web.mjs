fcdc696fadcc9087267e122572fa53f1aadb2d34
nin-jin <nin-jin@users.noreply.github.com>
2026-02-06 07:49:47 +0000

    Deploying to gh-pages from @ hyoo-ru/match.hyoo.ru@edc23323b5f43e26e5f70de303f5e5a2763209a0 🚀

--- web.mjs
+++ web.mjs
@@ -17447,6 +17447,8 @@ var $;
                 array = new Uint8Array(array);
             if (typeof array === 'string')
                 array = $mol_base64_ae_decode(array);
+            if (!ArrayBuffer.isView(array))
+                array = new Uint8Array(array);
             return new this(array.buffer, array.byteOffset, array.byteLength);
         }
         static toString() {
